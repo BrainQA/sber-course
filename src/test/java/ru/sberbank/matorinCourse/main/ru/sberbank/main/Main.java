85d30bc5a0fc1fa8346ab8ee8db54d77c9f2ba4c
Love.P <love.pazyak@gmail.com>
2019-09-13 09:44:39 +0300

    sdhjf

--- src/test/java/ru/sberbank/matorinCourse/main/ru/sberbank/main/Main.java
+++ src/test/java/ru/sberbank/matorinCourse/main/ru/sberbank/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         String hello = HelloWorld.sayHello("Alex");
         System.out.println(hello);
+        System.out.println();
     }
 }
