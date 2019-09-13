2233c28a82214fad7bd7214a270add09107b654a
Love.P <love.pazyak@gmail.com>
2019-09-13 09:41:37 +0300

    Initial commit

---
+++ src/test/java/ru/sberbank/matorinCourse/hello/ru/sberbank/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package ru.sberbank.matorinCourse.hello.ru.sberbank.hello;
+
+public class HelloWorld {
+
+    public static String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/test/java/ru/sberbank/matorinCourse/main/ru/sberbank/main/Main.java
@@ -0,0 +1,10 @@
+package ru.sberbank.matorinCourse.main.ru.sberbank.main;
+
+import ru.sberbank.matorinCourse.hello.ru.sberbank.hello.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = HelloWorld.sayHello("Alex");
+        System.out.println(hello);
+    }
+}
